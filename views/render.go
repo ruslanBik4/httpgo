2b585b853e50d9217b571957d01f0bc5a8c4f1de
Ruslan <bik4ruslan@gmail.com>
2017-01-05 13:43:36 +0200

    Add events for field in showAnyForm

--- views/render.go
+++ views/render.go
@@ -12,21 +12,31 @@ import (
 	"github.com/ruslanBik4/httpgo/models/users"
 )
 
+//noinspection GoInvalidConstType
+var HEADERS = map[string] string {
+	"Content-Type": "text/html; charset=utf-8",
+	"author":	"uStudio",
+}
+func WriteHeaders(w http.ResponseWriter) {
+	// выдаем стандартные заголовки страницы
+	for key, value := range HEADERS {
+		w.Header().Set(key, value)
+	}
+}
 func RenderTemplate(w http.ResponseWriter, r *http.Request, tmplName string, Content interface{} ) error {
-
 	var menu db.MenuItems
+
+	WriteHeaders(w)
 
 	headPage  := &layouts.HeadHTMLPage{
 		Charset: "charset=utf-8",
 		Language: "ru",
-		Title: "rcgthbvtyn gj ds;bdfyb. dhfujd jn yfc",
+		Title: "Заголовок новой страницы",
 	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	switch tmplName {
 	case "index":
 		var p *pages.IndexPageBody = Content.(*pages.IndexPageBody)
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 		headPage.Title = "введите Ваше имя отчество фамилию"
 
@@ -67,9 +77,24 @@ func RenderTemplate(w http.ResponseWriter, r *http.Request, tmplName string, Con
 		}
 		fmt.Fprint(w, headPage.HeadHTML())
 		fmt.Fprint(w, p.ShowAdminPage(""))
-
 	default:
 		fmt.Fprint(w, "no rendering with page %s with data %v", tmplName, Content)
 	}
 	return nil
+}
+
+func RenderAnyForm(w http.ResponseWriter, r *http.Request, Title string, fields forms.FieldsTable,
+			Inputs map[string] string ) error  {
+
+	WriteHeaders(w)
+	if Inputs != nil {
+		fmt.Fprint(w, layouts.DadataHead())
+	}
+	fmt.Fprint(w, fields.ShowAnyForm("/admin/exec/", Title))
+	if Inputs != nil {
+		fmt.Fprint(w, layouts.DadataScript(Inputs))
+	}
+
+	return nil
+
 }
