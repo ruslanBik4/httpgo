7b3429fe56e4aad8ba7fdaae1aef9b41577e8a47
ruslanBik4 <bik4ruslan@gmail.com>
2017-02-27 14:20:52 +0200

    Refactoring docx/reader

--- models/docx/reader.go
+++ models/docx/reader.go
@@ -3,20 +3,31 @@ package docx
 import (
 	"github.com/nguyenthenguyen/docx"
 	"log"
+	"flag"
 )
 
-func ReadDocx(fileName string) string {
-	if r, err := docx.ReadDocxFile(fileName); err != nil {
+var (
+	f_web    = flag.String("web","/home/travel/thetravel/web/","path to web files")
+
+)
+func ReplaceDocx(input, output string, replaces map[string] string) bool {
+	if r, err := docx.ReadDocxFile(input); err != nil {
 		log.Println(err)
-		return ""
+		return false
 	} else {
 		docx1 := r.Editable()
-		docx1.Replace("«00» _____ 2016 г.", "«21» лютого 2017 г.", -1)
-		docx1.Replace("Иванова Ивана Ивановича,", "Бикчентаева Руслана Ильдаровича", -1)
-		docx1.WriteToFile("./new.docx")
+
+		for search, replace := range replaces {
+			docx1.Replace(search, replace, -1)
+		}
+
+		if err := docx1.WriteToFile(output); err != nil {
+			return false
+
+		}
 
 		r.Close()
 
-		return "./new.docx"
+		return true
 	}
 }
