b143d28fb9dc67beeb6c36590795b6210ed7b409
ruslanBik4 <bik4ruslan@gmail.com>
2017-03-06 18:21:08 +0200

    Refactoring tables_view - rejects on tags table.. - replaces it an div

--- models/docx/reader.go
+++ models/docx/reader.go
@@ -3,6 +3,7 @@ package docx
 import (
 	"github.com/nguyenthenguyen/docx"
 	"log"
+	"net/http"
 )
 
  func ReplaceDocx(input, output string, replaces map[string] string) bool {
@@ -25,4 +26,19 @@ import (
 
 		return true
 	}
+}
+
+func RenderReplaesDoc(w http.ResponseWriter, templatesName string, replaces map[string] string)  error {
+	if r, err := docx.ReadDocxFile(templatesName); err != nil {
+		log.Println(err)
+		return err
+	} else {
+		template := r.Editable()
+		for search, replace := range replaces {
+			template.Replace(search, replace, -1)
+		}
+		template.Write(w)
+	}
+
+	return nil
 }
