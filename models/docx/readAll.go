8b157f76afe3c9bb947a583643f546e5fcd1bd87
ruslanBik4 <bik4ruslan@gmail.com>
2017-03-07 20:24:45 +0200

    Add property   BeforeHtml AfterHtml for struct fieldStructure

--- models/docx/readAll.go
+++ models/docx/readAll.go
@@ -1,5 +1,14 @@
 package docx
 
 import (
- //"github.com/sajari/docconv"
+ "github.com/sajari/docconv"
+ "log"
 )
+
+func getPlainText(fileName string) string{
+  if responce, err := docconv.ConvertPath(fileName); err != nil {
+    log.Println(err)
+  } else {
+    return responce.Body
+  }
+}
