0e6e3b3dbb02af52aab292e5ff5ba2ed75fc548c
savtym <savtym@gmail.com>
2017-04-28 14:01:50 +0300

    [ADD] api-post

--- views/templates/common/textarea.qtpl.go
+++ views/templates/common/textarea.qtpl.go
@@ -22,42 +22,49 @@ func StreamRenderTextarea(qw422016 *qt422016.Writer, name string) {
 	//line views/templates/common/textarea.qtpl:2
 	qw422016.N().S(`
 
+<label class="c-app-textarea">
     <textarea class="c-textarea" id="`)
-	//line views/templates/common/textarea.qtpl:4
+	//line views/templates/common/textarea.qtpl:5
 	qw422016.E().S(name)
-	//line views/templates/common/textarea.qtpl:4
+	//line views/templates/common/textarea.qtpl:5
 	qw422016.N().S(`" name="`)
-	//line views/templates/common/textarea.qtpl:4
+	//line views/templates/common/textarea.qtpl:5
 	qw422016.E().S(name)
-	//line views/templates/common/textarea.qtpl:4
+	//line views/templates/common/textarea.qtpl:5
 	qw422016.N().S(`"></textarea>
+    <span>`)
+	//line views/templates/common/textarea.qtpl:6
+	qw422016.E().S(name)
+	//line views/templates/common/textarea.qtpl:6
+	qw422016.N().S(`</span>
+</label>
 
 `)
-//line views/templates/common/textarea.qtpl:6
+//line views/templates/common/textarea.qtpl:9
 }
 
-//line views/templates/common/textarea.qtpl:6
+//line views/templates/common/textarea.qtpl:9
 func WriteRenderTextarea(qq422016 qtio422016.Writer, name string) {
-	//line views/templates/common/textarea.qtpl:6
+	//line views/templates/common/textarea.qtpl:9
 	qw422016 := qt422016.AcquireWriter(qq422016)
-	//line views/templates/common/textarea.qtpl:6
+	//line views/templates/common/textarea.qtpl:9
 	StreamRenderTextarea(qw422016, name)
-	//line views/templates/common/textarea.qtpl:6
+	//line views/templates/common/textarea.qtpl:9
 	qt422016.ReleaseWriter(qw422016)
-//line views/templates/common/textarea.qtpl:6
+//line views/templates/common/textarea.qtpl:9
 }
 
-//line views/templates/common/textarea.qtpl:6
+//line views/templates/common/textarea.qtpl:9
 func RenderTextarea(name string) string {
-	//line views/templates/common/textarea.qtpl:6
+	//line views/templates/common/textarea.qtpl:9
 	qb422016 := qt422016.AcquireByteBuffer()
-	//line views/templates/common/textarea.qtpl:6
+	//line views/templates/common/textarea.qtpl:9
 	WriteRenderTextarea(qb422016, name)
-	//line views/templates/common/textarea.qtpl:6
+	//line views/templates/common/textarea.qtpl:9
 	qs422016 := string(qb422016.B)
-	//line views/templates/common/textarea.qtpl:6
+	//line views/templates/common/textarea.qtpl:9
 	qt422016.ReleaseByteBuffer(qb422016)
-	//line views/templates/common/textarea.qtpl:6
+	//line views/templates/common/textarea.qtpl:9
 	return qs422016
-//line views/templates/common/textarea.qtpl:6
+//line views/templates/common/textarea.qtpl:9
 }
