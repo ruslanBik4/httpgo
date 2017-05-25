69617a0ecf300601e0008bc7f38b3cc9d7acf36d
Sergey Litvinov <serg_l@ukr.net>
2017-05-25 18:16:39 +0300

    Change logs

--- models/logs/logs.go
+++ models/logs/logs.go
@@ -7,11 +7,11 @@ import (
 
 func DebugLog(title, data interface{}) {
     _, fn, line, _ := runtime.Caller(1)
-    log.Printf("[DEBUG], %s, in line %d.  %s= `%v`", fn, line, title, data)
+    log.Printf("[DEBUG], %s, in line %d.  %s=%v", fn, line, title, data)
 }
 
 func ErrorLog(err error, data interface{}) {
     _, fn, line, _ := runtime.Caller(1)
-    log.Printf("[ERROR] %s, in line %d. Error -  %v. Data= %v", fn, line, err, data)
+    log.Printf("[ERROR] %s, in line %d. Error -  %v. Data=%v", fn, line, err, data)
 }
 
