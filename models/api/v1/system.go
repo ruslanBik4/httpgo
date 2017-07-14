7223d2a898b1ba45be28dd443e2b56b7118af221
ruslanBik4 <bik4ruslan@gmail.com>
2017-07-14 18:24:01 +0300

    Add new system route /api/log/errors/ show errors

--- models/api/v1/system.go
+++ models/api/v1/system.go
@@ -38,13 +38,14 @@ func HandleRestartServer(w http.ResponseWriter, r *http.Request) {
 
 	cmd := exec.Command("systemctl", "restart", "httpgo")
 	cmd.Dir = ServerConfig.SystemPath()
+	cmd.Stdout = w
 
 	err := cmd.Start()
-	if err == nil {
-		if stdoutStderr, err := cmd.Output(); err == nil {
-			views.RenderOutput(w, stdoutStderr)
-		}
-	}
+	//if err == nil {
+	//	if stdoutStderr, err := cmd.Output(); err == nil {
+	//		views.RenderOutput(w, stdoutStderr)
+	//	}
+	//}
 	if err != nil {
 		views.RenderInternalError(w, err)
 	}
