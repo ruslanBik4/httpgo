4ef00bb6c321b4966e8ab45fefd10dfa3220c09f
ruslanBik4 <bik4ruslan@gmail.com>
2017-09-05 14:28:08 +0300

    [EDIT] fonts try return other font format if font not found

--- views/fonts/handler.go
+++ views/fonts/handler.go
@@ -11,6 +11,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"io"
+	"os"
+	"github.com/ruslanBik4/httpgo/views"
 )
 
 var PathWeb string
@@ -27,6 +30,12 @@ func contains(array []string, str string) bool {
 
 	return false
 }
+var fontTypes = map[string] string {
+	"ttf" : "font-sfnt",
+	"woff" : "x-woff",
+}
+// push font for some browser
+// @/fonts/{font_name}
 func HandleGetFont(w http.ResponseWriter, r *http.Request) {
 
 	//w.Header().Set("Content-Type", "mime/type; ttf")
@@ -35,9 +44,7 @@ func HandleGetFont(w http.ResponseWriter, r *http.Request) {
 	ext := ".ttf"
 	if browser := r.Header["User-Agent"]; contains(browser, "Safari") {
 		ext = ".woff"
-		w.Header().Set("Content-Type", "mime/type: font/x-woff")
 	} else {
-		w.Header().Set("Content-Type", "mime/type: font/font-sfnt")
 		//http.ServeFile(w, r, PathWeb+r.URL.Path+ext)
 		logs.DebugLog("browser=", browser)
 	}
@@ -46,9 +53,23 @@ func HandleGetFont(w http.ResponseWriter, r *http.Request) {
 	if pos := strings.Index(r.URL.Path, "."); pos > 0 {
 		filename = filename[:pos-1]
 	}
-	if data, err := ioutil.ReadFile(PathWeb + filename + ext); err != nil {
-		logs.ErrorLog(err)
-	} else {
-		w.Write(data)
+	data, err := ioutil.ReadFile(PathWeb + filename + ext)
+
+	if err != nil {
+		if os.IsNotExist(err) && (ext == ".woff") {
+			data, err = ioutil.ReadFile(PathWeb + filename + ".ttf")
+		}
+		if err != nil {
+			views.RenderInternalError(w, err)
+			return
+		}
 	}
+
+	setHeaderFromFontType(w, ext)
+	w.Write(data)
+
+}
+// set header on font type
+func setHeaderFromFontType(w http.ResponseWriter, ext string) {
+	w.Header().Set("Content-Type", "mime/type: font/" + fontTypes[ext[:1]])
 }
