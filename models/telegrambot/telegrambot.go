bba2a03c4ad0728b2020270ffffe8d019964e738
SargtLa <sargtischlerin@gmail.com>
2019-11-20 15:12:42 -0500

    added config reading to telegrambot

--- models/telegrambot/telegrambot.go
+++ models/telegrambot/telegrambot.go
@@ -4,18 +4,25 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"gopkg.in/yaml.v2"
+	"github.com/ruslanBik4/httpgo/logs"
 )
 
 type TelegramBot struct {
-	Token string
+	Token string `yaml:"BotToken"`
+	ChatID string `yaml:"ChatID"`
 }
 
+// TelegramRequest makes base url for request
 func (tbot TelegramBot) TelegramRequest(action string) string {
 	return ("https://api.telegram.org/bot" + tbot.Token + "/" + action + "?")
 }
 
-func (tbot TelegramBot) SendError(message string, chatID string, markdown bool) {
-	sendRequestURL := (tbot.TelegramRequest("sendMessage") + "?chat_id=" + chatID + "&text=" + message)
+// SendMessage is used for sending messages
+func (tbot TelegramBot) SendMessage(message string, markdown bool) {
+	sendRequestURL := (tbot.TelegramRequest("sendMessage") + "chat_id=" + tbot.ChatID + "&text=" + message)
+
+	// For using bold and italic font in message sent
 	if markdown {
 		sendRequestURL += "&parse_mode=Markdown"
 	}
@@ -23,18 +30,34 @@ func (tbot TelegramBot) SendError(message string, chatID string, markdown bool) 
 	log.Println(MakeRequest(sendRequestURL))
 }
 
+// MakeRequest executes request and gets response? converting it to string
 func MakeRequest(url string) string {
 	resp, err := http.Get(url)
 
 	if err != nil {
-		log.Fatalln(err)
+		logs.ErrorLog(err, "TelegramBot")
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
-		log.Fatalln(err)
+		logs.ErrorLog(err, "TelegramBot")
 	}
 
 	return string(body)
+}
+
+// GetNewTelegramBot reads a config file for bot token and chatID and creates new TelegramBot struct
+func GetNewTelegramBot(confPath string) (tb *TelegramBot, err error) {
+
+	yamlFile, err := ioutil.ReadFile(confPath)
+	if err != nil {
+		logs.ErrorLog(err)
+	}
+	err = yaml.Unmarshal(yamlFile, &tb)
+	if err != nil {
+		logs.ErrorLog(err)
+	}
+
+	return
 }
