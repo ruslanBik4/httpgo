39364f334ac1ef08068547ba6196ee0d9c34cbda
ruslanBik4 <bik4ruslan@gmail.com>
2021-01-22 12:01:07 +0200

    [MOD] api params add withCORs fix

--- apis/apiroute.go
+++ apis/apiroute.go
@@ -110,6 +110,12 @@ func (route *ApiRoute) CheckAndRun(ctx *fasthttp.RequestCtx, fncAuth FncAuth) (r
 		return nil, errRouteOnlyLocal
 	}
 
+	if route.WithCors {
+		ctx.Response.Header.Set("Access-Control-Allow-Origin", "*")
+		ctx.Response.Header.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, X-Auth-Token, Origin, Authorization, X-Requested-With")
+		ctx.Response.Header.Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	}
+
 	if bytes.HasPrefix(ctx.Request.Header.ContentType(), []byte(ctJSON)) && (route.DTO != nil) {
 		return route.performsJSON(ctx)
 	}
@@ -205,12 +211,6 @@ func (route *ApiRoute) performsJSON(ctx *fasthttp.RequestCtx) (interface{}, erro
 	}
 
 	ctx.SetUserValue(JSONParams, dto)
-
-	if route.WithCors {
-		ctx.Response.Header.Set("Access-Control-Allow-Origin", "*")
-		ctx.Response.Header.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
-		ctx.Response.Header.Set("Access-Control-Allow-Methods", "POST, GET")
-	}
 
 	return route.Fnc(ctx)
 }
