70ecdebe4c8589674af3abeb2162f793560eea6e
ruslanBik4 <bik4ruslan@gmail.com>
2021-02-27 12:58:20 +0200

    [MOD] any json  - ADD PGX TYPE ENCODING

--- views/templates/json/anyjson.go
+++ views/templates/json/anyjson.go
@@ -7,9 +7,13 @@ package json
 
 import (
 	"database/sql"
+	"math"
+	"math/big"
 	"unsafe"
 
+	"github.com/jackc/pgtype"
 	jsoniter "github.com/json-iterator/go"
+	"github.com/ruslanBik4/logs"
 	"github.com/valyala/quicktemplate"
 )
 
@@ -91,4 +95,194 @@ func init() {
 		func(pointer unsafe.Pointer) bool {
 			return false
 		})
+
+	jsoniter.RegisterTypeEncoderFunc("pgtype.Int4Array",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			accArray := (*pgtype.Int4Array)(ptr)
+			stream.WriteArrayStart()
+
+			for i, val := range accArray.Elements {
+				if i > 0 {
+					stream.WriteMore()
+				}
+				stream.WriteInt32(val.Int)
+			}
+
+			stream.WriteArrayEnd()
+
+		},
+		func(pointer unsafe.Pointer) bool {
+			return false
+		})
+
+	jsoniter.RegisterTypeEncoderFunc("pgtype.Int8Array",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			accArray := (*pgtype.Int8Array)(ptr)
+			stream.WriteArrayStart()
+
+			for i, val := range accArray.Elements {
+				if i > 0 {
+					stream.WriteMore()
+				}
+				stream.WriteInt64(val.Int)
+			}
+
+			stream.WriteArrayEnd()
+
+		},
+		func(pointer unsafe.Pointer) bool {
+			return false
+		})
+
+	jsoniter.RegisterTypeEncoderFunc("pgtype.Float4Array",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			accArray := (*pgtype.Float4Array)(ptr)
+			stream.WriteArrayStart()
+
+			for i, val := range accArray.Elements {
+				if i > 0 {
+					stream.WriteMore()
+				}
+				stream.WriteFloat32Lossy(val.Float)
+			}
+
+			stream.WriteArrayEnd()
+
+		},
+		func(pointer unsafe.Pointer) bool {
+			return false
+		})
+
+	jsoniter.RegisterTypeEncoderFunc("pgtype.Float8Array",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			accArray := (*pgtype.Float8Array)(ptr)
+			stream.WriteArrayStart()
+
+			for i, val := range accArray.Elements {
+				if i > 0 {
+					stream.WriteMore()
+				}
+				stream.WriteFloat64Lossy(val.Float)
+			}
+
+			stream.WriteArrayEnd()
+
+		},
+		func(pointer unsafe.Pointer) bool {
+			return false
+		})
+
+	jsoniter.RegisterTypeEncoderFunc("pgtype.Numeric",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			val := (*pgtype.Numeric)(ptr)
+			divider := math.Pow10(int(val.Exp))
+			stream.WriteFloat64Lossy(float64(val.Int.Int64()) * divider)
+		},
+		func(pointer unsafe.Pointer) bool {
+			return false
+		})
+
+	jsoniter.RegisterTypeDecoderFunc("pgtype.Numeric",
+		func(ptr unsafe.Pointer, iter *jsoniter.Iterator) {
+			val := iter.ReadFloat64()
+
+			f := big.NewFloat(val)
+			err := (*(*pgtype.Numeric)(ptr)).Scan(f.String())
+			if err != nil {
+				logs.ErrorLog(err, val)
+			}
+		})
+
+	jsoniter.RegisterTypeEncoderFunc("pgtype.NumericArray",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			accArray := (*pgtype.NumericArray)(ptr)
+			stream.WriteArrayStart()
+
+			for i, val := range accArray.Elements {
+				if i > 0 {
+					stream.WriteMore()
+				}
+				divider := math.Pow10(int(val.Exp))
+				stream.WriteFloat64Lossy(float64(val.Int.Int64()) * divider)
+			}
+
+			stream.WriteArrayEnd()
+
+		},
+		func(pointer unsafe.Pointer) bool {
+			return false
+		})
+
+	jsoniter.RegisterTypeEncoderFunc("pgtype.Daterange",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			val := (*pgtype.Daterange)(ptr)
+			stream.WriteArrayStart()
+
+			stream.WriteInt64(val.Lower.Time.Unix())
+			stream.WriteMore()
+			stream.WriteInt64(val.Upper.Time.Unix())
+
+			stream.WriteArrayEnd()
+		},
+		func(pointer unsafe.Pointer) bool {
+			return false
+		})
+
+	jsoniter.RegisterTypeEncoderFunc("pgtype.VarcharArray",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			accArray := (*pgtype.VarcharArray)(ptr)
+			stream.WriteArrayStart()
+
+			for i, val := range accArray.Elements {
+				if i > 0 {
+					stream.WriteMore()
+				}
+				stream.WriteString(val.String)
+			}
+
+			stream.WriteArrayEnd()
+
+		},
+		func(pointer unsafe.Pointer) bool {
+			return false
+		})
+
+	jsoniter.RegisterTypeEncoderFunc("pgtype.BPCharArray",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			accArray := (*pgtype.BPCharArray)(ptr)
+			stream.WriteArrayStart()
+
+			for i, val := range accArray.Elements {
+				if i > 0 {
+					stream.WriteMore()
+				}
+				stream.WriteString(val.String)
+			}
+
+			stream.WriteArrayEnd()
+
+		},
+		func(pointer unsafe.Pointer) bool {
+			return false
+		})
+
+	jsoniter.RegisterTypeEncoderFunc("pgtype.TextArray",
+		func(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+			accArray := (*pgtype.TextArray)(ptr)
+			stream.WriteArrayStart()
+
+			for i, val := range accArray.Elements {
+				if i > 0 {
+					stream.WriteMore()
+				}
+				stream.WriteString(val.String)
+			}
+
+			stream.WriteArrayEnd()
+
+		},
+		func(pointer unsafe.Pointer) bool {
+			return false
+		})
+
 }
