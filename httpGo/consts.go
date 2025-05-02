4394c6fa2afcf6a9141a7af85f3875be23cfa115
Руслан Бикчентаев <BIk4ruslan@gmail.com>
2025-05-02 11:45:17 +0300

    [FIX] table renderigng hidden column during refresh [ADD] version info for makefile other projects

--- httpGo/consts.go
+++ httpGo/consts.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023. Author: Ruslan Bikchentaev. All rights reserved.
+ * Copyright (c) 2023-2025. Author: Ruslan Bikchentaev. All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  * Перший приватний програміст.
@@ -7,6 +7,20 @@
 
 package httpGo
 
-// todo auto changing version
-// httpgo version
-const httpgoVersion = "1.2.1"
+import "fmt"
+
+const ShowVersion = "/api/version()"
+
+// version
+var (
+	Version   string
+	HTTPGOVer string
+	GoVersion string
+	OSVersion string
+	Build     string
+	Branch    string
+)
+
+func GetAppTitle(name string) string {
+	return fmt.Sprintf("%s (%s) Version: %s, Build Time: %s", name, Branch, Version, Build)
+}
